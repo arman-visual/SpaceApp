001d02a95ad7526158c49274cf62898b206c9c43
Arman <arman.agora@gmail.com>
2021-02-17 23:00:22 +0100

    Example call endpoint

--- app/src/main/java/com/avisual/spaceapp/MainActivity.kt
+++ app/src/main/java/com/avisual/spaceapp/MainActivity.kt
@@ -1,13 +1,26 @@
 package com.avisual.spaceapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.avisual.spaceapp.databinding.ActivityMainBinding
+import com.avisual.spaceapp.model.NasaClient
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        lifecycleScope.launch {
+            var apiKey = getString(R.string.api_key)
+            var photosResult = NasaClient.service.listPhotosByEarthDate("2018-6-3", apiKey)
+            var listaPhotos = photosResult.photos
+            if (listaPhotos.size != null) {
+                println("Lista de objtetos de tamaño : ${listaPhotos.size}")
+            } else println("NO hay nada.")
+
+        }
     }
 }
