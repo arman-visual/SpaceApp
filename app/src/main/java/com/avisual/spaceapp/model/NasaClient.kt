6750a5b6620257c1ac52d825b1f983d9a86f4e57
ArmanMac <Arman.agora@gmail.com>
2021-02-21 17:40:04 +0100

    Modified Retrofit Client for 2 baseUrl
    Se ha modificado la clase NasaClient porque solo habrá 2 url a las que consumiremos datos.

--- app/src/main/java/com/avisual/spaceapp/model/NasaClient.kt
+++ app/src/main/java/com/avisual/spaceapp/model/NasaClient.kt
@@ -4,17 +4,17 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object NasaClient {
-    private val retrofitRover = Retrofit.Builder()
-            .baseUrl("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+    private val retrofitNasa = Retrofit.Builder()
+        .baseUrl("https://api.nasa.gov/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
 
     private val retrofitLibrary = Retrofit.Builder()
         .baseUrl("https://images-api.nasa.gov/")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
-    val roverService: NasaRoverService = retrofitRover.create(NasaRoverService::class.java)
+    val roverService: NasaService = retrofitNasa.create(NasaService::class.java)
 
     val libraryService: NasaLibraryService = retrofitLibrary.create(NasaLibraryService::class.java)
 }
