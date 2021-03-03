3ae5302877ee2f88a876b0f543ef664071e1d318
Arman <arman.agora@gmail.com>
2021-03-03 15:40:12 +0100

    Modified classes for Parcelize

--- app/src/main/java/com/avisual/spaceapp/model/nasaLibraryResponse/Link.kt
+++ app/src/main/java/com/avisual/spaceapp/model/nasaLibraryResponse/Link.kt
@@ -1,7 +1,11 @@
 package com.avisual.spaceapp.model.nasaLibraryResponse
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Link(
     val href: String,
     val rel: String,
     val render: String
-)
+): Parcelable
