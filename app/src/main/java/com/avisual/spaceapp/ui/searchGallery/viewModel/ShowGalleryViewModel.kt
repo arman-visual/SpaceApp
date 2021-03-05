c7016158d9d04e37e7a9ed1740cf3a73bda2ac2e
Arman <arman.agora@gmail.com>
2021-03-05 12:43:05 +0100

    Add ViewModelFactory for ShowGalleryViewModel
    Se ha incluido una Factory para nuestro ViewModel dado que recibe parámetros(Repository).

--- app/src/main/java/com/avisual/spaceapp/ui/searchGallery/viewModel/ShowGalleryViewModel.kt
+++ app/src/main/java/com/avisual/spaceapp/ui/searchGallery/viewModel/ShowGalleryViewModel.kt
@@ -4,10 +4,11 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.avisual.spaceapp.common.ScopeViewModel
 import com.avisual.spaceapp.model.nasaLibraryResponse.Item
+import com.avisual.spaceapp.repository.PhotoGalleryRepository
 import com.avisual.spaceapp.server.NasaClient
 import kotlinx.coroutines.launch
 
-class ExploreGalleryViewModel : ScopeViewModel() {
+class ShowGalleryViewModel(private val photoGalleryRepository: PhotoGalleryRepository) : ScopeViewModel() {
 
     private val _photosLibrary = MutableLiveData<List<Item>>()
     val photosLibrary: LiveData<List<Item>>
@@ -18,7 +19,8 @@ class ExploreGalleryViewModel : ScopeViewModel() {
     }
 
     private fun refresh() = launch {
-        var response = NasaClient.libraryService.searchContain("Neptune")
+       // var response = NasaClient.libraryService.searchContain("Neptune")
+        var response = photoGalleryRepository.findPhotosGallery("Neptune")
         _photosLibrary.value = response.collection.items
     }
 }
