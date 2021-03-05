af76d58893ea1cd35a3e2859a6b00c2902fd4aa3
Arman <arman.agora@gmail.com>
2021-03-05 20:33:59 +0100

    Added Search Toolbar and his item and menu XML in fragment
    Se ha añadido un toolbar para el fragment explore gallery dado que realizaremos una búsqueda en esta vista para poder observar más imágenes.

--- app/src/main/java/com/avisual/spaceapp/ui/searchGallery/ShowGalleryFragment.kt
+++ app/src/main/java/com/avisual/spaceapp/ui/searchGallery/ShowGalleryFragment.kt
@@ -2,15 +2,14 @@ package com.avisual.spaceapp.ui.searchGallery
 
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import com.avisual.spaceapp.R
 import com.avisual.spaceapp.databinding.FragmentExploreGalleryBinding
 import com.avisual.spaceapp.model.nasaLibraryResponse.Item
 import com.avisual.spaceapp.repository.PhotoGalleryRepository
@@ -24,6 +23,12 @@ class ShowGalleryFragment : Fragment() {
     private lateinit var viewModel: ShowGalleryViewModel
     private lateinit var photosAdapter: GalleryPhotosAdapter
     private lateinit var navController: NavController
+    private lateinit var menuNav: MenuItem
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,6 +44,11 @@ class ShowGalleryFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         navController = view.findNavController()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_nav_gallery, menu)
+        super.onCreateOptionsMenu(menu, inflater)
     }
 
     private fun setUpUi() {
