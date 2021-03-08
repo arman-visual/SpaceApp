f8722420de2bfadfa317327b12b52564d85aacd8
Arman <arman.agora@gmail.com>
2021-03-08 17:54:16 +0100

    Modified file for incloud a method extension.
    -Transformaremos Item-PhotoGallery(Servidor) a PhotoGallery(Room) para elegir que datos guardar en esta tabla personalizada.

--- app/src/main/java/com/avisual/spaceapp/model/nasaLibraryResponse/Item.kt
+++ app/src/main/java/com/avisual/spaceapp/model/nasaLibraryResponse/Item.kt
@@ -1,6 +1,7 @@
 package com.avisual.spaceapp.model.nasaLibraryResponse
 
 import android.os.Parcelable
+import com.avisual.spaceapp.database.PhotoGallery
 import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 
@@ -10,4 +11,21 @@ data class Item(
     val data_photo: List<Data>,
     val href: String,
     val links: List<Link>
-): Parcelable
+) : Parcelable
+
+
+fun Item.convertToPhotoGallery() = PhotoGallery(
+    0,
+    this.links[0].href,
+    this.data_photo[0].center,
+    this.data_photo[0].date_created,
+    this.data_photo[0].description,
+    this.data_photo[0].description_508,
+    this.data_photo[0].keywords,
+    this.data_photo[0].location,
+    this.data_photo[0].media_type,
+    this.data_photo[0].nasa_id,
+    this.data_photo[0].photographer,
+    this.data_photo[0].secondary_creator,
+    this.data_photo[0].title
+)
