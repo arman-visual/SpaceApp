58f47d0565b326868ff9c06d2fe8aa9e94f6e58c
Arman <arman.agora@gmail.com>
2021-03-08 22:24:46 +0100

    Modified fragment for include in viewmodel repository with DB
    -Añadido en viewmodel repositorio con DB de parametro.
    -capturando el boton de favorito en la vista.

--- app/src/main/java/com/avisual/spaceapp/ui/searchGallery/DetailPhotoGalleryFragment.kt
+++ app/src/main/java/com/avisual/spaceapp/ui/searchGallery/DetailPhotoGalleryFragment.kt
@@ -5,9 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
+import com.avisual.spaceapp.database.Db
 import com.avisual.spaceapp.databinding.FragmentDetailPhotoGalleryBinding
 import com.avisual.spaceapp.model.nasaLibraryResponse.Item
+import com.avisual.spaceapp.model.nasaLibraryResponse.convertToPhotoGallery
+import com.avisual.spaceapp.repository.PhotoGalleryRepository
+import com.avisual.spaceapp.ui.searchGallery.viewModel.SavedPhotosViewModel
+import com.avisual.spaceapp.ui.searchGallery.viewModel.SavedPhotosViewModelFactory
 import com.bumptech.glide.Glide
 
 
@@ -15,6 +21,8 @@ class DetailPhotoGalleryFragment : Fragment() {
 
     private val args: DetailPhotoGalleryFragmentArgs by navArgs()
     private lateinit var photo: Item
+    private lateinit var viewModel: SavedPhotosViewModel
+    private lateinit var photoGalleryRepository: PhotoGalleryRepository
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,10 +31,20 @@ class DetailPhotoGalleryFragment : Fragment() {
         val binding = FragmentDetailPhotoGalleryBinding.inflate(layoutInflater)
         photo = args.photoArg!!
 
+        viewModel = buildViewModel()
+
+        binding.btFavorite.setOnClickListener { viewModel.clickOnfavoritePhoto(photo.convertToPhotoGallery()) }
+
         Glide.with(binding.root.context).load(photo.links[0].href).into(binding.imagePhoto)
         binding.titlePhotoDetail.text = photo.data_photo[0].title
         binding.descriptionPhotoDetail.text = photo.data_photo[0].description
         return binding.root
     }
 
+    private fun buildViewModel(): SavedPhotosViewModel {
+        val database = Db.getDatabase(requireContext())
+        photoGalleryRepository = PhotoGalleryRepository(database)
+        val factory = SavedPhotosViewModelFactory(photoGalleryRepository)
+        return ViewModelProvider(this, factory).get(SavedPhotosViewModel::class.java)
+    }
 }
