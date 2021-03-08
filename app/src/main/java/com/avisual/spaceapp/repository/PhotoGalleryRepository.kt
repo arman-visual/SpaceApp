5480f0d7c2599a7d336cdf4903167404a0ed9a43
Arman <arman.agora@gmail.com>
2021-03-08 17:55:20 +0100

    Added method in Repository for saveFavorite
    Se ha incluido en el repositorio un nuevo método que servirá para guardar en Base de Datos una Foto(PhotoGallery)

--- app/src/main/java/com/avisual/spaceapp/repository/PhotoGalleryRepository.kt
+++ app/src/main/java/com/avisual/spaceapp/repository/PhotoGalleryRepository.kt
@@ -1,11 +1,20 @@
 package com.avisual.spaceapp.repository
 
+import com.avisual.spaceapp.database.Db
+import com.avisual.spaceapp.database.PhotoGallery
+import com.avisual.spaceapp.database.PhotoGalleryDao
 import com.avisual.spaceapp.model.nasaLibraryResponse.CollectionNasaResult
 import com.avisual.spaceapp.server.NasaClient
 
-class PhotoGalleryRepository {
+class PhotoGalleryRepository(database: Db) {
+
+    private var photoGalleryDao: PhotoGalleryDao = database.photoDao()
 
     suspend fun findPhotosGallery(keyword: String): CollectionNasaResult {
         return NasaClient.libraryService.searchContain(keyword)
+    }
+
+    suspend fun saveFavoritePhoto(photoGallery: PhotoGallery) {
+        photoGalleryDao.insert(photoGallery)
     }
 }
