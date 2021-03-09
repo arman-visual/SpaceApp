ef79fad8eed9ed010148e4e88b67a750aa73e6d4
ArmanMac <Arman.agora@gmail.com>
2021-03-09 01:05:43 +0100

    Modified classes for save in Room Database
    Se han modificado las clases para poder guardar información en Room.
    -Método de extension y val -> var

--- app/src/main/java/com/avisual/spaceapp/database/PhotoGallery.kt
+++ app/src/main/java/com/avisual/spaceapp/database/PhotoGallery.kt
@@ -6,16 +6,12 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "photos_gallery")
 data class PhotoGallery (
     @PrimaryKey(autoGenerate = true)
-    val id: Int,
-    val url:String,
-    val center: String,
-    val date_created: String,
-    val description: String,
-    val description_508: String,
-    val location: String,
-    val media_type: String,
-    val nasa_id: String,
-    val photographer: String,
-    val secondary_creator: String,
-    val title: String
+    var id: Int,
+    var url:String,
+    var date_created: String,
+    var description: String,
+    var media_type: String,
+    var nasa_id: String,
+    var photographer: String,
+    var title: String
 )
