75dbd6f494a82b1d0dc5f3f71f9349a43e4b93aa
Arman <arman.agora@gmail.com>
2021-03-09 12:05:14 +0100

    Removed args unnecessary in this ViewModel

--- app/src/main/java/com/avisual/spaceapp/ui/searchGallery/viewModel/DetailPhotoViewModel.kt
+++ app/src/main/java/com/avisual/spaceapp/ui/searchGallery/viewModel/DetailPhotoViewModel.kt
@@ -1,7 +1,5 @@
 package com.avisual.spaceapp.ui.searchGallery.viewModel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import com.avisual.spaceapp.common.ScopeViewModel
 import com.avisual.spaceapp.database.PhotoGallery
 import com.avisual.spaceapp.repository.PhotoGalleryRepository
@@ -9,11 +7,6 @@ import kotlinx.coroutines.launch
 
 class DetailPhotoViewModel(private val photoGalleryRepository: PhotoGalleryRepository) :
     ScopeViewModel() {
-
-    private val _savedPhotos = MutableLiveData<List<PhotoGallery>>(emptyList())
-    val savedPhotos: LiveData<List<PhotoGallery>>
-        get() = _savedPhotos
-
 
     fun saveFavoritePhoto(photoGallery: PhotoGallery) {
         launch {
