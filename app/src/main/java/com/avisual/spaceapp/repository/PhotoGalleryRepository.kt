fc9a079184ebc5204b440ce942b43b65208c6157
Arman <arman.agora@gmail.com>
2021-03-12 20:06:37 +0100

    Added methods getByNasaId and modified id -> nasa_id
    Se han modificado los métodos del DAO para apuntar a nuestro nuevo nasa_id para las fotos.

--- app/src/main/java/com/avisual/spaceapp/repository/PhotoGalleryRepository.kt
+++ app/src/main/java/com/avisual/spaceapp/repository/PhotoGalleryRepository.kt
@@ -2,8 +2,8 @@ package com.avisual.spaceapp.repository
 
 import androidx.lifecycle.LiveData
 import com.avisual.spaceapp.database.Db
-import com.avisual.spaceapp.model.PhotoGallery
 import com.avisual.spaceapp.database.PhotoGalleryDao
+import com.avisual.spaceapp.model.PhotoGallery
 import com.avisual.spaceapp.model.nasaLibraryResponse.CollectionNasaResult
 import com.avisual.spaceapp.server.NasaClient
 import kotlinx.coroutines.Dispatchers
@@ -21,6 +21,10 @@ class PhotoGalleryRepository(database: Db) {
         photoGalleryDao.insert(photoGallery)
     }
 
+    suspend fun deletePhoto(photoGallery: PhotoGallery) = withContext(Dispatchers.IO) {
+        photoGalleryDao.delete(photoGallery)
+    }
+
     suspend fun getAllPhotos(): List<PhotoGallery> = withContext(Dispatchers.IO) {
         photoGalleryDao.getAll()
     }
@@ -29,7 +33,7 @@ class PhotoGalleryRepository(database: Db) {
         return photoGalleryDao.getAllLiveData()
     }
 
-    suspend fun getFindById(nasaId: String): PhotoGallery = withContext(Dispatchers.IO) {
-        photoGalleryDao.getByNasaID(nasaId)
+    suspend fun getFindByNasaId(nasaId: String): PhotoGallery? = withContext(Dispatchers.IO) {
+        photoGalleryDao.getByNasaId(nasaId)
     }
 }
