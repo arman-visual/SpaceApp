3c559af0f27b2ffd795ee6e406aaedda263a9ad4
Arman <arman.agora@gmail.com>
2021-03-14 21:56:37 +0100

    Configured swipe to delete photo
    Se ha configurado el swipe para borrar una fotografía en BD.

--- app/src/main/java/com/avisual/spaceapp/ui/searchGallery/SavedPhotosFragment.kt
+++ app/src/main/java/com/avisual/spaceapp/ui/searchGallery/SavedPhotosFragment.kt
@@ -7,9 +7,11 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
 import com.avisual.spaceapp.database.Db
-import com.avisual.spaceapp.model.PhotoGallery
 import com.avisual.spaceapp.databinding.FragmentSavedPhotosBinding
+import com.avisual.spaceapp.model.PhotoGallery
 import com.avisual.spaceapp.repository.PhotoGalleryRepository
 import com.avisual.spaceapp.ui.searchGallery.adapter.SavedPhotosAdapter
 import com.avisual.spaceapp.ui.searchGallery.viewModel.SavedPhotosViewModel
@@ -49,7 +51,11 @@ class SavedPhotosFragment : Fragment() {
             navToDetailPhoto(it)
         }
         binding.recycler.adapter = photosAdapter
+        val callback = itemTouchHelperConfigure()
+        val itemTouchHelper = ItemTouchHelper(callback)
+        itemTouchHelper.attachToRecyclerView(binding.recycler)
     }
+
 
     private fun subscribeUi() {
         viewModel.saveLivePhotos.observe(requireActivity()) {
@@ -58,7 +64,41 @@ class SavedPhotosFragment : Fragment() {
     }
 
     private fun navToDetailPhoto(photo: PhotoGallery) {
-        val action = SavedPhotosFragmentDirections.actionSavedPhotosFragmentToDetailPhotoGalleryFragment(photo)
+        val action =
+            SavedPhotosFragmentDirections.actionSavedPhotosFragmentToDetailPhotoGalleryFragment(
+                photo
+            )
         findNavController().navigate(action)
+    }
+
+    private fun itemTouchHelperConfigure() = object : ItemTouchHelper.Callback() {
+        override fun getMovementFlags(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder
+        ): Int {
+            val dragFlags = ItemTouchHelper.UP or ItemTouchHelper.DOWN
+            val swipeFlags = ItemTouchHelper.START or ItemTouchHelper.END
+            return makeMovementFlags(dragFlags, swipeFlags)
+        }
+
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ): Boolean {
+            if (viewHolder.itemViewType != target.itemViewType) {
+                return false
+            }
+            return photosAdapter.onChangePhotoPosition(
+                viewHolder.adapterPosition,
+                target.adapterPosition
+            )
+        }
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            val position = viewHolder.adapterPosition
+            val photo = photosAdapter.getPhotoPosition(position)
+            viewModel.deletePhoto(photo)
+        }
     }
 }
--- app/src/main/java/com/avisual/spaceapp/ui/searchGallery/viewModel/SavedPhotosViewModel.kt
+++ app/src/main/java/com/avisual/spaceapp/ui/searchGallery/viewModel/SavedPhotosViewModel.kt
@@ -1,11 +1,19 @@
 package com.avisual.spaceapp.ui.searchGallery.viewModel
 
 import com.avisual.spaceapp.common.ScopeViewModel
+import com.avisual.spaceapp.model.PhotoGallery
 import com.avisual.spaceapp.repository.PhotoGalleryRepository
+import kotlinx.coroutines.launch
 
 class SavedPhotosViewModel(private val photoGalleryRepository: PhotoGalleryRepository) :
     ScopeViewModel() {
 
     val saveLivePhotos = photoGalleryRepository.getAllPhotosLiveData()
+
+    fun deletePhoto(photoGallery: PhotoGallery) {
+        launch {
+            photoGalleryRepository.deletePhoto(photoGallery)
+        }
+    }
 
 }
