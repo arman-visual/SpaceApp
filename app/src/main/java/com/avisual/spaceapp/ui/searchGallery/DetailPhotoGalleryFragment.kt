81799ad5d9e71d5e86460d03acefd76ba82c55f1
Arman <arman.agora@gmail.com>
2021-03-17 11:55:33 +0100

    Added control of permission functions
    Se ha añadido funciones para el control del permiso requerido por nuestra app, siguiendo la documentacion de android.

--- app/src/main/java/com/avisual/spaceapp/ui/searchGallery/DetailPhotoGalleryFragment.kt
+++ app/src/main/java/com/avisual/spaceapp/ui/searchGallery/DetailPhotoGalleryFragment.kt
@@ -1,9 +1,18 @@
 package com.avisual.spaceapp.ui.searchGallery
 
+import android.Manifest
+import android.annotation.TargetApi
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
@@ -58,6 +67,7 @@ class DetailPhotoGalleryFragment : Fragment() {
         binding.titlePhotoDetail.text = photo.title
         binding.descriptionPhotoDetail.text = photo.description
         binding.fbtSaveFavorite.setOnClickListener { viewModel.changeSaveStatusOfPhoto(photo) }
+        binding.fbtDownload.setOnClickListener { checkVersion() }
     }
 
     private fun subscribeUi() {
@@ -69,6 +79,80 @@ class DetailPhotoGalleryFragment : Fragment() {
             }
             binding.fbtSaveFavorite.setImageResource(drawableRes)
         }
+    }
+
+    private fun checkVersion() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            askPermissions()
+        } else {
+            downloadImage(photo.url)
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    fun askPermissions() {
+        if (ContextCompat.checkSelfPermission(
+                requireActivity(),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            //Este Metodo comprueba si anteriormente se solicito este permiso
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    requireActivity(),
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                )
+            ) {
+                AlertDialog.Builder(requireActivity())
+                    .setTitle("Permission required")
+                    .setMessage("Permission required to save photos from the Web.")
+                    .setPositiveButton("Allow") { dialog, id ->
+                        ActivityCompat.requestPermissions(
+                            requireActivity(),
+                            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                            MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE
+                        )
+                        requireActivity().finish()
+                    }
+                    .setNegativeButton("Deny") { dialog, id -> dialog.cancel() }
+                    .show()
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(
+                    requireActivity(),
+                    arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                    MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE
+                )
+            }
+        } else {
+            // Permission has already been granted
+            downloadImage(photo.url)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    Log.i("PERMISO", "NO VACIO y OTORGADO")
+                    downloadImage(photo.url)
+                }
+                return
+            }
+        }
+    }
+
+    private fun downloadImage(url: String) {
+        Toast.makeText(requireContext(), "Descargando....${url}", Toast.LENGTH_LONG).show()
+    }
+
+    companion object {
+        private const val MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 1
     }
 
 }
