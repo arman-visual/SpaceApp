7f8bd36050fc7e3c7610e6f0cc02a280e50f3e39
Arman <arman.agora@gmail.com>
2021-03-17 12:09:44 +0100

    Added new implementation for download image
    Se ha implementado la lógica para la descarga de imagen indicada.(De manera temporal la dejamos en el fragment, dado que lo ideal es que estuviera en el ViewModel).

--- app/src/main/java/com/avisual/spaceapp/ui/searchGallery/DetailPhotoGalleryFragment.kt
+++ app/src/main/java/com/avisual/spaceapp/ui/searchGallery/DetailPhotoGalleryFragment.kt
@@ -2,9 +2,14 @@ package com.avisual.spaceapp.ui.searchGallery
 
 import android.Manifest
 import android.annotation.TargetApi
+import android.app.DownloadManager
+import android.content.Context
 import android.content.pm.PackageManager
+import android.database.Cursor
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -15,6 +20,7 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
 import com.avisual.spaceapp.R
 import com.avisual.spaceapp.common.loadUrl
@@ -24,6 +30,10 @@ import com.avisual.spaceapp.model.PhotoGallery
 import com.avisual.spaceapp.repository.PhotoGalleryRepository
 import com.avisual.spaceapp.ui.searchGallery.viewModel.DetailPhotoViewModel
 import com.avisual.spaceapp.ui.searchGallery.viewModel.DetailPhotoViewModelFactory
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.File
 
 
 class DetailPhotoGalleryFragment : Fragment() {
@@ -148,7 +158,72 @@ class DetailPhotoGalleryFragment : Fragment() {
     }
 
     private fun downloadImage(url: String) {
-        Toast.makeText(requireContext(), "Descargando....${url}", Toast.LENGTH_LONG).show()
+        var msg: String? = ""
+        var lastMsg = ""
+
+        val directory = File(Environment.DIRECTORY_DOWNLOADS)
+
+        if (!directory.exists()) {
+            directory.mkdirs()
+        }
+
+        val downloadManager =
+            requireActivity().getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+
+        val downloadUri = Uri.parse(url)
+
+        val request = DownloadManager.Request(downloadUri).apply {
+            setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI or DownloadManager.Request.NETWORK_MOBILE)
+                .setAllowedOverRoaming(false)
+                .setTitle(url.substring(url.lastIndexOf("/") + 1))
+                .setDescription("")
+                .setDestinationInExternalPublicDir(
+                    directory.toString(),
+                    url.substring(url.lastIndexOf("/") + 1)
+                )
+        }
+
+        val downloadId = downloadManager.enqueue(request)
+        val query = DownloadManager.Query().setFilterById(downloadId)
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            var downloading = true
+            while (downloading) {
+                val cursor: Cursor = downloadManager.query(query)
+                cursor.moveToFirst()
+                if (cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS)) == DownloadManager.STATUS_SUCCESSFUL) {
+                    downloading = false
+                }
+                val status = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS))
+                msg = statusMessage(url, directory, status)
+                if (msg != lastMsg) {
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(
+                            requireActivity(),
+                            msg,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                    lastMsg = msg ?: ""
+                }
+                cursor.close()
+            }
+        }
+    }
+
+    private fun statusMessage(url: String, directory: File, status: Int): String? {
+        var msg = ""
+        msg = when (status) {
+            DownloadManager.STATUS_FAILED -> "Download has been failed, please try again"
+            DownloadManager.STATUS_PAUSED -> "Paused"
+            DownloadManager.STATUS_PENDING -> "Pending"
+            DownloadManager.STATUS_RUNNING -> "Downloading..."
+            DownloadManager.STATUS_SUCCESSFUL -> "Image downloaded successfully in $directory" + File.separator + url.substring(
+                url.lastIndexOf("/") + 1
+            )
+            else -> "There's nothing to download"
+        }
+        return msg
     }
 
     companion object {
