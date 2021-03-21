7a68d2749d607b70ea575362340ddf2b356a45d3
Arman <arman.agora@gmail.com>
2021-03-21 19:01:46 +0100

    Added PhotoCuriosity
    Se ha convertido la Photo que recibimos del servidor a una clase nueva que llamamos PhotoCuriosity que utilizaremos para nuestro Adapter y manejar en Base de datos.

---
+++ app/src/main/java/com/avisual/spaceapp/model/PhotoCuriosity.kt
@@ -0,0 +1,23 @@
+package com.avisual.spaceapp.model
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+@Entity(tableName = "photos_curiosity")
+@Parcelize
+data class PhotoCuriosity(
+    @PrimaryKey
+    val id: Int,
+    val img_src: String,
+    val full_name: String,
+    val camera_name: String,
+    val rover_id: Int,
+    val earth_date: String,
+    val landing_date: String,
+    val launch_date: String,
+    val rover_name: String,
+    val status: String,
+    val sol: Int
+) : Parcelable
