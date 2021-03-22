93b253ce37de5a92683cd98205182524d928bb92
Arman <arman.agora@gmail.com>
2021-03-22 12:57:32 +0100

    Modified Fragment for call method from viewModel
    Se ha añadido una llamada al método del viewModel para obtener fotos por una fecha por DEFAULT y visualizar nuestro recycler view

--- app/src/main/java/com/avisual/spaceapp/ui/roverMars/ShowPhotosFragment.kt
+++ app/src/main/java/com/avisual/spaceapp/ui/roverMars/ShowPhotosFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.avisual.spaceapp.R
 import com.avisual.spaceapp.databinding.FragmentSavedPhotosBinding
+import com.avisual.spaceapp.databinding.FragmentShowPhotosBinding
 import com.avisual.spaceapp.repository.PhotoCuriosityRepository
 import com.avisual.spaceapp.ui.roverMars.adapter.PhotosRoverAdapter
 import com.avisual.spaceapp.ui.roverMars.viewModel.ShowPhotosViewModel
@@ -14,7 +16,7 @@ import com.avisual.spaceapp.ui.roverMars.viewModel.ShowPhotosViewModelFactory
 
 class ShowPhotosFragment : Fragment() {
 
-    private lateinit var binding: FragmentSavedPhotosBinding
+    private lateinit var binding: FragmentShowPhotosBinding
     private lateinit var adapter: PhotosRoverAdapter
     private lateinit var viewModel: ShowPhotosViewModel
     private lateinit var photoCuriosityRepository: PhotoCuriosityRepository
@@ -25,7 +27,7 @@ class ShowPhotosFragment : Fragment() {
     ): View? {
         buildDependencies()
         viewModel = buildViewModel()
-        binding = FragmentSavedPhotosBinding.inflate(layoutInflater)
+        binding = FragmentShowPhotosBinding.inflate(layoutInflater)
         return binding.root
     }
 
@@ -33,6 +35,8 @@ class ShowPhotosFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         adapter = PhotosRoverAdapter(emptyList())
         binding.recycler.adapter = adapter
+
+        viewModel.findPhotosByDate(date = "2015-6-3", apiKey = getString(R.string.api_key))
         subscribeUi()
     }
 
