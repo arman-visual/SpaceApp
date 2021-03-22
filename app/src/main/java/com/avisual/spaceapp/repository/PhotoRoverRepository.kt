7aeaf3a827aa024996fa58d720ef4d0c15312522
Arman <arman.agora@gmail.com>
2021-03-22 19:50:26 +0100

    Modified name PhotoCuriosity to PhotoRover and the same with Repository
    Se ha modificado el nombre de las clases a PhotoRover y PhotoRoverRepository.

--- app/src/main/java/com/avisual/spaceapp/repository/PhotoRoverRepository.kt
+++ app/src/main/java/com/avisual/spaceapp/repository/PhotoRoverRepository.kt
@@ -3,7 +3,7 @@ package com.avisual.spaceapp.repository
 import com.avisual.spaceapp.model.nasaRoverResponse.RoverPhotosResult
 import com.avisual.spaceapp.server.NasaClient
 
-class PhotoCuriosityRepository {
+class PhotoRoverRepository {
 
     suspend fun findPhotosRoverFromServer(date: String, apiKey: String): RoverPhotosResult {
         return NasaClient.nasaService.marsRoverPhotosByEarthDate(date, apiKey)
