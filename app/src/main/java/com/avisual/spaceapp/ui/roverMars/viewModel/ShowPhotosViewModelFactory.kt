339b16901b29b7e393e41c5d4b0bb353fa94b30d
Arman <arman.agora@gmail.com>
2021-03-22 19:50:26 +0100

    Modified name PhotoCuriosity to PhotoRover and the same with Repository
    Se ha modificado el nombre de las clases a PhotoRover y PhotoRoverRepository.

--- app/src/main/java/com/avisual/spaceapp/ui/roverMars/viewModel/ShowPhotosViewModelFactory.kt
+++ app/src/main/java/com/avisual/spaceapp/ui/roverMars/viewModel/ShowPhotosViewModelFactory.kt
@@ -2,11 +2,11 @@ package com.avisual.spaceapp.ui.roverMars.viewModel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.avisual.spaceapp.repository.PhotoCuriosityRepository
+import com.avisual.spaceapp.repository.PhotoRoverRepository
 
 class ShowPhotosViewModelFactory constructor(
-    private val photoCuriosityRepository: PhotoCuriosityRepository
+    private val photoRoverRepository: PhotoRoverRepository
 ) : ViewModelProvider.NewInstanceFactory() {
     override fun <T : ViewModel> create(modelClass: Class<T>) =
-        ShowPhotosViewModel(photoCuriosityRepository) as T
+        ShowPhotosViewModel(photoRoverRepository) as T
 }
