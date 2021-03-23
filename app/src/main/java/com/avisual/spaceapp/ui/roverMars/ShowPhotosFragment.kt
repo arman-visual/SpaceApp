96e89945872444bed4e3ef2854682b12d956948f
Arman <arman.agora@gmail.com>
2021-03-23 14:13:20 +0100

    Modified subscribe from model
    Se ha modificado la manera de recibir los cambios desde nuestro model.

--- app/src/main/java/com/avisual/spaceapp/ui/roverMars/ShowPhotosFragment.kt
+++ app/src/main/java/com/avisual/spaceapp/ui/roverMars/ShowPhotosFragment.kt
@@ -4,20 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.avisual.spaceapp.R
 import com.avisual.spaceapp.databinding.FragmentShowPhotosBinding
 import com.avisual.spaceapp.repository.PhotoRoverRepository
 import com.avisual.spaceapp.ui.roverMars.adapter.PhotosRoverAdapter
+import com.avisual.spaceapp.ui.roverMars.viewModel.ShowPhotosUi
 import com.avisual.spaceapp.ui.roverMars.viewModel.ShowPhotosViewModel
 import com.avisual.spaceapp.ui.roverMars.viewModel.ShowPhotosViewModelFactory
 
 class ShowPhotosFragment : Fragment() {
 
-    companion object{
+    companion object {
         private const val DEFAULT_DATE_EARTH = "2012-08-06"
     }
+
     private lateinit var binding: FragmentShowPhotosBinding
     private lateinit var adapter: PhotosRoverAdapter
     private lateinit var viewModel: ShowPhotosViewModel
@@ -29,6 +33,7 @@ class ShowPhotosFragment : Fragment() {
     ): View? {
         buildDependencies()
         viewModel = buildViewModel()
+        viewModel.findPhotosByDate(DEFAULT_DATE_EARTH, apiKey = getString(R.string.api_key))
         binding = FragmentShowPhotosBinding.inflate(layoutInflater)
         return binding.root
     }
@@ -37,7 +42,6 @@ class ShowPhotosFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         adapter = PhotosRoverAdapter(emptyList())
         binding.recycler.adapter = adapter
-        viewModel.findPhotosByDate(DEFAULT_DATE_EARTH, apiKey = getString(R.string.api_key))
         binding.button.setOnClickListener { onClickSearchButton() }
         subscribeUi()
     }
@@ -61,8 +65,14 @@ class ShowPhotosFragment : Fragment() {
     }
 
     private fun subscribeUi() {
-        viewModel.photosRover.observe(requireActivity()) {
-            adapter.setItems(it)
-        }
+        viewModel.model.observe(requireActivity(), Observer(::updateUi))
+    }
+
+    private fun updateUi(model: ShowPhotosUi) {
+        binding.progressBarM.visibility =
+            if (model is ShowPhotosUi.Loading) View.VISIBLE else View.GONE
+
+        val photos = if (model is ShowPhotosUi.Content) model.photos else emptyList()
+        adapter.setItems(photos)
     }
 }
