5bf8b481c436a856e1c23b5caba9f46791b239d5
Arman <arman.agora@gmail.com>
2021-03-23 18:22:30 +0100

    Modified NasaClient for connection OkHttp
    Se ha modificado la clase NasaClient, añadiéndole un "clientSetup" para hacer que las peticiones a este servidor puedan abarcar hasta 30 segundos de espera.
    
    Por defecto Retrofit lo tiene a 10 segundos, y en algunos casos nuestras peticiones requerirán más tiempo.

--- app/src/main/java/com/avisual/spaceapp/server/NasaClient.kt
+++ app/src/main/java/com/avisual/spaceapp/server/NasaClient.kt
@@ -1,12 +1,22 @@
 package com.avisual.spaceapp.server
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object NasaClient {
+
+    private val clientSetup = OkHttpClient.Builder()
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .writeTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .build()
+
     private val retrofitNasa = Retrofit.Builder()
         .baseUrl("https://api.nasa.gov/")
         .addConverterFactory(GsonConverterFactory.create())
+        .client(clientSetup)
         .build()
 
     private val retrofitLibrary = Retrofit.Builder()
