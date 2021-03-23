c2d72a10b9452e74407ca122a2029c79d35092c7
Arman <arman.agora@gmail.com>
2021-03-23 13:14:29 +0100

    Modified Repository in correct thread in courutines
    Se ha añadido el hilo donde se ha de ejecutar esta corrutina correctamente.

--- app/src/main/java/com/avisual/spaceapp/repository/PhotoRoverRepository.kt
+++ app/src/main/java/com/avisual/spaceapp/repository/PhotoRoverRepository.kt
@@ -2,11 +2,14 @@ package com.avisual.spaceapp.repository
 
 import com.avisual.spaceapp.model.nasaRoverResponse.RoverPhotosResult
 import com.avisual.spaceapp.server.NasaClient
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class PhotoRoverRepository {
 
-    suspend fun findPhotosRoverFromServer(date: String, apiKey: String): RoverPhotosResult {
-        return NasaClient.nasaService.marsRoverPhotosByEarthDate(date, apiKey)
-    }
+    suspend fun findPhotosRoverFromServer(date: String, apiKey: String): RoverPhotosResult =
+        withContext(Dispatchers.IO) {
+            NasaClient.nasaService.marsRoverPhotosByEarthDate(date, apiKey)
+        }
 
 }
