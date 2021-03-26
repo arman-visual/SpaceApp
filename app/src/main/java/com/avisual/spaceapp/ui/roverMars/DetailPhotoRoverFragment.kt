c4d43c076ecc588ba8ad58fe3b31bd2d138d6fdb
Arman <arman.agora@gmail.com>
2021-03-26 17:49:48 +0100

    Added simple design in this detail screen and cofigure back buttom
    Se ha añadido diseño sencillo al XML por ahora y configurado correctamente la navegación y los datos que queremos enseñar. También hemos configurado el botón de "Atras" para navegar correctamente entre fragments.

--- app/src/main/java/com/avisual/spaceapp/ui/roverMars/DetailPhotoRoverFragment.kt
+++ app/src/main/java/com/avisual/spaceapp/ui/roverMars/DetailPhotoRoverFragment.kt
@@ -4,20 +4,64 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
-import com.avisual.spaceapp.R
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.avisual.spaceapp.common.loadUrl
 import com.avisual.spaceapp.databinding.FragmentDetailPhotoRoverBinding
+import com.avisual.spaceapp.model.PhotoRover
+import com.avisual.spaceapp.repository.PhotoRoverRepository
+import com.avisual.spaceapp.ui.roverMars.viewModel.DetailPhotoRoverViewModel
+import com.avisual.spaceapp.ui.roverMars.viewModel.DetailRoverPhotoViewModelFactory
 
 
 class DetailPhotoRoverFragment : Fragment() {
 
+    private val args: DetailPhotoRoverFragmentArgs by navArgs()
     private lateinit var binding: FragmentDetailPhotoRoverBinding
+    private lateinit var viewModel: DetailPhotoRoverViewModel
+    private lateinit var photoRoverRepository: PhotoRoverRepository
+    private lateinit var photo: PhotoRover
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentDetailPhotoRoverBinding.inflate(layoutInflater)
+        photo = args.photoRoverArgs!!
+        buildDependencies()
+        viewModel = buildViewModel()
+        setUpUi()
         return binding.root
     }
 
+    private fun buildDependencies() {
+        photoRoverRepository = PhotoRoverRepository()
+    }
+
+    private fun buildViewModel(): DetailPhotoRoverViewModel {
+        val factory = DetailRoverPhotoViewModelFactory(photoRoverRepository)
+        return ViewModelProvider(this, factory).get(DetailPhotoRoverViewModel::class.java)
+    }
+
+    private fun setUpUi() {
+        binding = FragmentDetailPhotoRoverBinding.inflate(layoutInflater)
+
+        configureOnBackPressed()
+
+        binding.apply {
+            imagePhoto.loadUrl(photo.img_src)
+            titlePhotoDetail.text = photo.full_name
+            descriptionPhotoDetail.text = photo.rover_name
+        }
+    }
+
+    private fun configureOnBackPressed() {
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+            val action =
+                DetailPhotoRoverFragmentDirections.actionDetailPhotoRoverFragmentToShowPhotosFragment()
+            findNavController().navigate(action)
+        }
+    }
 }
