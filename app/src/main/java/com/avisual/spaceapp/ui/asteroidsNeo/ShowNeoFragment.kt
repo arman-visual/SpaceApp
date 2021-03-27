eef46c15e8609a0ead181241486ad3488999aec8
Arman <arman.agora@gmail.com>
2021-03-27 12:01:31 +0100

    Added factory for viewModel and use repository
    Se ha añadido factory para construir el viewModel y usar Repository dentro del mismo.

--- app/src/main/java/com/avisual/spaceapp/ui/asteroidsNeo/ShowNeoFragment.kt
+++ app/src/main/java/com/avisual/spaceapp/ui/asteroidsNeo/ShowNeoFragment.kt
@@ -6,21 +6,44 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.avisual.spaceapp.R
 import com.avisual.spaceapp.databinding.ShowNeoFragmentBinding
+import com.avisual.spaceapp.repository.NeoRepository
 import com.avisual.spaceapp.ui.asteroidsNeo.viewModel.ShowNeoViewModel
+import com.avisual.spaceapp.ui.asteroidsNeo.viewModel.ShowNeoViewModelFactory
+import com.avisual.spaceapp.ui.roverMars.viewModel.ShowPhotosViewModel
 
 class ShowNeoFragment : Fragment() {
 
     private lateinit var viewModel: ShowNeoViewModel
     private lateinit var binding: ShowNeoFragmentBinding
+    private lateinit var neoRepository: NeoRepository
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        viewModel = ViewModelProvider(this).get(ShowNeoViewModel::class.java)
+        dependencies()
+        viewModel = buildViewModel()
         binding = ShowNeoFragmentBinding.inflate(layoutInflater)
         binding.text.text = "NEO"
         return binding.root
     }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.getAsteroidsByDate("2021-02-21", "2021-02-27", getString(R.string.api_key))
+    }
+
+    private fun dependencies() {
+        neoRepository = NeoRepository()
+    }
+
+    private fun buildViewModel(): ShowNeoViewModel {
+        val factory = ShowNeoViewModelFactory(neoRepository)
+        return ViewModelProvider(this, factory).get(ShowNeoViewModel::class.java)
+    }
+
 
 }
--- app/src/main/java/com/avisual/spaceapp/ui/asteroidsNeo/viewModel/ShowNeoViewModel.kt
+++ app/src/main/java/com/avisual/spaceapp/ui/asteroidsNeo/viewModel/ShowNeoViewModel.kt
@@ -1,7 +1,16 @@
 package com.avisual.spaceapp.ui.asteroidsNeo.viewModel
 
 import com.avisual.spaceapp.common.ScopeViewModel
+import com.avisual.spaceapp.repository.NeoRepository
+import kotlinx.coroutines.launch
 
-class ShowNeoViewModel : ScopeViewModel() {
-    // TODO: Implement the ViewModel
+class ShowNeoViewModel(var neoRepository: NeoRepository) : ScopeViewModel() {
+
+    fun getAsteroidsByDate(dateStart: String, dateFinal:String, apiKey:String){
+        launch {
+            var response = neoRepository.findPhotosRoverFromServer(dateStart, dateFinal, apiKey)
+            var orderDay = response.registerDay.toSortedMap()
+        }
+    }
+
 }
