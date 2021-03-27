800f5edc460133490f71a510378f0f838b0f23f4
Arman <arman.agora@gmail.com>
2021-03-27 12:02:07 +0100

    Added provisional Adapter
    Añadido sin código útil por ahora.

---
+++ app/src/main/java/com/avisual/spaceapp/ui/asteroidsNeo/adapter/AsteroidsNeoAdapter.kt
@@ -0,0 +1,22 @@
+package com.avisual.spaceapp.ui.asteroidsNeo.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.avisual.spaceapp.databinding.ShowNeoFragmentBinding
+
+class AsteroidsNeoAdapter() : RecyclerView.Adapter<AsteroidsNeoAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO()
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    class ViewHolder(var binding: ShowNeoFragmentBinding) : RecyclerView.ViewHolder(binding.root)
+}
