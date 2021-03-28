e1500a2285f0eb9e30357b52a15bcdde3979ce3f
Arman <arman.agora@gmail.com>
2021-03-28 21:13:57 +0200

    Modified viewModel for contain new list of neo
    Se ha añadido en el ViewModel la lógica para recibir un Map y transformarlo en una nueva Lista que incluya nuestro Neo (dominio) ordenado por la fecha.

--- app/src/main/java/com/avisual/spaceapp/ui/asteroidsNeo/viewModel/ShowNeoViewModel.kt
+++ app/src/main/java/com/avisual/spaceapp/ui/asteroidsNeo/viewModel/ShowNeoViewModel.kt
@@ -1,15 +1,33 @@
 package com.avisual.spaceapp.ui.asteroidsNeo.viewModel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.avisual.spaceapp.common.ScopeViewModel
+import com.avisual.spaceapp.model.Neo
+import com.avisual.spaceapp.model.asteroidsNeoWsResponse.toNeo
 import com.avisual.spaceapp.repository.NeoRepository
 import kotlinx.coroutines.launch
 
 class ShowNeoViewModel(var neoRepository: NeoRepository) : ScopeViewModel() {
 
-    fun getAsteroidsByDate(dateStart: String, dateFinal:String, apiKey:String){
+    private val _listsAsteroids = MutableLiveData<List<Neo>>()
+    val listAsteroids: LiveData<List<Neo>> get() = _listsAsteroids
+
+    fun getAsteroidsByDate(dateStart: String, dateFinal: String, apiKey: String) {
+
+        var totalAsteroids = mutableListOf<Neo>()
+
         launch {
-            var response = neoRepository.findPhotosRoverFromServer(dateStart, dateFinal, apiKey)
-            var orderDay = response.registerDay.toSortedMap()
+            val response = neoRepository.findPhotosRoverFromServer(dateStart, dateFinal, apiKey)
+
+            val totalDays = response.registerDay.size
+            var keys = response.registerDay.keys.sorted()
+
+            for (i in 0 until totalDays) {
+                response.registerDay.getValue(keys[i]).map { neo -> totalAsteroids.add(neo.toNeo(keys[i])) }
+            }
+            println("Total de objetos almacenados : ${totalAsteroids.toList().size}")
+            _listsAsteroids.postValue(totalAsteroids)
         }
     }
 
