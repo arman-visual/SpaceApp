c30197a6f6cd1692d7050ab1571104c04307fafe
Arman <arman.agora@gmail.com>
2021-03-29 21:31:54 +0200

    Try configure Calendar from material design
    Intentando adaptar el calendario de MD en nuestro fragment.

--- app/src/main/java/com/avisual/spaceapp/ui/asteroidsNeo/ShowNeoFragment.kt
+++ app/src/main/java/com/avisual/spaceapp/ui/asteroidsNeo/ShowNeoFragment.kt
@@ -4,16 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.avisual.spaceapp.R
 import com.avisual.spaceapp.databinding.ShowNeoFragmentBinding
-import com.avisual.spaceapp.model.Neo
 import com.avisual.spaceapp.repository.NeoRepository
 import com.avisual.spaceapp.ui.asteroidsNeo.adapter.AsteroidsNeoAdapter
 import com.avisual.spaceapp.ui.asteroidsNeo.viewModel.ShowNeoViewModel
 import com.avisual.spaceapp.ui.asteroidsNeo.viewModel.ShowNeoViewModelFactory
-import com.avisual.spaceapp.ui.roverMars.viewModel.ShowPhotosViewModel
+import com.google.android.material.datepicker.CalendarConstraints
+import com.google.android.material.datepicker.MaterialDatePicker
+import java.util.*
 
 class ShowNeoFragment : Fragment() {
 
@@ -21,6 +23,10 @@ class ShowNeoFragment : Fragment() {
     private lateinit var binding: ShowNeoFragmentBinding
     private lateinit var neoRepository: NeoRepository
     private lateinit var adapter: AsteroidsNeoAdapter
+    private lateinit var datePicker:MaterialDatePicker<Long>
+
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,8 +34,40 @@ class ShowNeoFragment : Fragment() {
     ): View? {
         dependencies()
         viewModel = buildViewModel()
+        configureCalendar()
         binding = ShowNeoFragmentBinding.inflate(layoutInflater)
+
+        binding.buttonCalendar.setOnClickListener {
+            datePicker.show(requireActivity().supportFragmentManager, "DATA_PICKER")
+            datePicker.addOnPositiveButtonClickListener {
+                Toast.makeText(requireContext(), datePicker.headerText, Toast.LENGTH_SHORT).show()
+            }
+        }
         return binding.root
+    }
+
+    private fun configureCalendar() {
+        //Calendar
+        val today = MaterialDatePicker.todayInUtcMilliseconds()
+        val calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+
+        calendar.timeInMillis = today
+        calendar[Calendar.MONTH] = Calendar.JANUARY
+        val janThisYear = calendar.timeInMillis
+
+        calendar.timeInMillis = today
+        calendar[Calendar.MONTH] = Calendar.DECEMBER
+        val decThisYear = calendar.timeInMillis
+
+        val constraintsBuilder =
+            CalendarConstraints.Builder()
+                .setStart(janThisYear)
+                .setEnd(decThisYear)
+        datePicker = MaterialDatePicker.Builder.datePicker()
+            .setTitleText("Select date")
+            .setCalendarConstraints(constraintsBuilder.build())
+            .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+            .build()
     }
 
 
@@ -42,7 +80,7 @@ class ShowNeoFragment : Fragment() {
     }
 
     private fun subscribe() {
-        viewModel.listAsteroids.observe(requireActivity()){
+        viewModel.listAsteroids.observe(requireActivity()) {
             adapter.setItems(it)
         }
     }
