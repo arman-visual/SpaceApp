4e2d14e669f1c941f1fb0432fb9fc590e52f6fbd
Arman <arman.agora@gmail.com>
2021-04-02 22:12:08 +0200

    Added method in Dao for getById

--- app/src/main/java/com/avisual/spaceapp/database/AsteroidDao.kt
+++ app/src/main/java/com/avisual/spaceapp/database/AsteroidDao.kt
@@ -3,12 +3,16 @@ package com.avisual.spaceapp.database
 import androidx.lifecycle.LiveData
 import androidx.room.*
 import com.avisual.spaceapp.model.Neo
+import com.avisual.spaceapp.model.PhotoGallery
 
 @Dao
 interface AsteroidDao {
 
     @Query("SELECT * FROM  neo_asteroids_saved ORDER BY id DESC")
     fun getAllLiveData(): LiveData<List<Neo>>
+
+    @Query("SELECT * FROM neo_asteroids_saved WHERE id = :asteroidId")
+    suspend fun getById(asteroidId: String): Neo?
 
     @Delete
     suspend fun delete(asteroid: Neo)
