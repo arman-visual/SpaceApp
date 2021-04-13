d5d0ed87499e2fd5432cf90ac6a8d0b09a5a1580
Arman <arman.agora@gmail.com>
2021-04-13 11:37:24 +0200

    Removed some old method for request permissions to user
    Se ha quitado código que de forma manual y según la documentación de android solicita permisos.
    Se ha agregado la nueva forma de pedir permisos con dexter.

--- app/src/main/java/com/avisual/spaceapp/ui/searchGallery/DetailPhotoGalleryFragment.kt
+++ app/src/main/java/com/avisual/spaceapp/ui/searchGallery/DetailPhotoGalleryFragment.kt
@@ -1,26 +1,20 @@
 package com.avisual.spaceapp.ui.searchGallery
 
 import android.Manifest
-import android.annotation.TargetApi
 import android.app.DownloadManager
 import android.content.Context
-import android.content.Intent
-import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
-import android.provider.Settings
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AlertDialog
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
+import com.avisual.spaceapp.PermissionRequester
 import com.avisual.spaceapp.R
 import com.avisual.spaceapp.common.loadUrl
 import com.avisual.spaceapp.database.Db
@@ -29,16 +23,10 @@ import com.avisual.spaceapp.model.PhotoGallery
 import com.avisual.spaceapp.repository.PhotoGalleryRepository
 import com.avisual.spaceapp.ui.searchGallery.viewModel.DetailPhotoViewModel
 import com.avisual.spaceapp.ui.searchGallery.viewModel.DetailPhotoViewModelFactory
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import com.karumi.dexter.Dexter
-import com.karumi.dexter.PermissionToken
-import com.karumi.dexter.listener.PermissionDeniedResponse
-import com.karumi.dexter.listener.PermissionGrantedResponse
-import com.karumi.dexter.listener.PermissionRequest
-import com.karumi.dexter.listener.single.PermissionListener
 import java.io.File
 
 
+@RequiresApi(Build.VERSION_CODES.Q)
 class DetailPhotoGalleryFragment : Fragment() {
 
     private val args: DetailPhotoGalleryFragmentArgs by navArgs()
@@ -46,6 +34,21 @@ class DetailPhotoGalleryFragment : Fragment() {
     private lateinit var photo: PhotoGallery
     private lateinit var viewModel: DetailPhotoViewModel
     private lateinit var photoGalleryRepository: PhotoGalleryRepository
+
+    private val oldPermissionStorageRequester by lazy {
+        PermissionRequester(
+            requireActivity(),
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+    }
+
+    private val newPermissionAccessMediaRequester by lazy {
+        PermissionRequester(
+            requireActivity(),
+            Manifest.permission.ACCESS_MEDIA_LOCATION
+        )
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -80,8 +83,26 @@ class DetailPhotoGalleryFragment : Fragment() {
         binding.titlePhotoDetail.text = photo.title
         binding.descriptionPhotoDetail.text = photo.description
         binding.fbtSaveFavorite.setOnClickListener { viewModel.changeSaveStatusOfPhoto(photo) }
-        binding.fbtDownload.setOnClickListener { useDexterRequestPermission() }
+        binding.fbtDownload.setOnClickListener { useDexterFar() }
     }
+
+    private fun useDexterFar() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            oldPermissionStorageRequester.request { isGranted ->
+                if (isGranted) {
+                    downloadImageDManager(photo.url)
+                }
+            }
+        } else {
+            newPermissionAccessMediaRequester.request {
+                if (it){
+                    downloadImageDManager(photo.url)
+                }
+            }
+        }
+    }
+
 
     private fun subscribeUi() {
         viewModel.statusFavorite.observe(requireActivity()) { isSaved ->
@@ -91,72 +112,6 @@ class DetailPhotoGalleryFragment : Fragment() {
                 R.drawable.photo_no_saved
             }
             binding.fbtSaveFavorite.setImageResource(drawableRes)
-        }
-    }
-
-    private fun checkVersion() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
-            askPermissions()
-        } else {
-            downloadImageDManager(photo.url)
-        }
-    }
-
-    @TargetApi(Build.VERSION_CODES.M)
-    fun askPermissions() {
-        if (ContextCompat.checkSelfPermission(
-                requireActivity(),
-                Manifest.permission.WRITE_EXTERNAL_STORAGE
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-
-            //Este Metodo comprueba si anteriormente se solicito este permiso
-            if (ActivityCompat.shouldShowRequestPermissionRationale(
-                    requireActivity(),
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE
-                )
-            ) {
-                AlertDialog.Builder(requireActivity())
-                    .setTitle("Permission required")
-                    .setMessage("Permission required to save photos from the Web.")
-                    .setPositiveButton("Allow") { dialog, id ->
-                        ActivityCompat.requestPermissions(
-                            requireActivity(),
-                            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
-                            MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE
-                        )
-                        requireActivity().finish()
-                    }
-                    .setNegativeButton("Deny") { dialog, id -> dialog.cancel() }
-                    .show()
-            } else {
-                // No explanation needed, we can request the permission.
-                ActivityCompat.requestPermissions(
-                    requireActivity(),
-                    arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
-                    MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE
-                )
-            }
-        } else {
-            // Permission has already been granted
-            downloadImageDManager(photo.url)
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        when (requestCode) {
-            MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE -> {
-                // If request is cancelled, the result arrays are empty.
-                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    Log.i("PERMISO", "NO VACIO y OTORGADO")
-                    downloadImageDManager(photo.url)
-                }
-                return
-            }
         }
     }
 
@@ -183,46 +138,4 @@ class DetailPhotoGalleryFragment : Fragment() {
             requireActivity().getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
         downloadManager.enqueue(request)
     }
-
-    /**
-     * Dexter
-     */
-
-    private fun useDexterRequestPermission() {
-        Dexter
-            .withActivity(requireActivity())
-            .withPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-            .withListener(object : PermissionListener {
-                override fun onPermissionGranted(response: PermissionGrantedResponse?) {
-                    downloadImageDManager(photo.url)
-                }
-
-                override fun onPermissionDenied(response: PermissionDeniedResponse?) {
-                    MaterialAlertDialogBuilder(requireActivity())
-                        .setTitle("Permission required")
-                        .setMessage("Permission required to save photos from the Web.")
-                        .setPositiveButton("GOTO SETTINGS") { dialog, wich ->
-                            dialog.cancel()
-                            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-                            val uri = Uri.fromParts("package", requireActivity().packageName, null)
-                            intent.data = uri
-                            startActivity(intent)
-                        }
-                        .show()
-                }
-
-                override fun onPermissionRationaleShouldBeShown(
-                    permission: PermissionRequest?,
-                    token: PermissionToken?
-                ) {
-                    token?.continuePermissionRequest()
-                }
-            })
-            .check()
-    }
-
-    companion object {
-        private const val MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 1
-    }
-
 }
