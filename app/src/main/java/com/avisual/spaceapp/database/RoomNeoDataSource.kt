a306399d6a22e914d1e5324c657cdbf7a956b946
Arman <arman.agora@gmail.com>
2021-04-22 17:55:24 +0200

    Added interface inside data module(LocalDataSource and RemoteDataSource)
    Se han añadido las interfaces para obtener los datos de Local y Remoto para el objeto "NEO"

---
+++ app/src/main/java/com/avisual/spaceapp/database/RoomNeoDataSource.kt
@@ -0,0 +1,17 @@
+package com.avisual.spaceapp.database
+
+import com.avisual.data.source.NeoLocalDataSource
+import com.avisual.spaceapp.server.toDomainNeo
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import com.avisual.domain.Neo as DomainNeo
+
+class RoomNeoDataSource(private val db: Db) : NeoLocalDataSource {
+
+    private val neoDao = db.asteroidDao()
+
+    override suspend fun getStoredNeos(): Flow<List<DomainNeo>> =
+        neoDao.getNeoWithFlow().map { listNeoFramework ->
+            listNeoFramework.map { frameworkNeo -> frameworkNeo.toDomainNeo() }
+        }
+}
---
+++ app/src/main/java/com/avisual/spaceapp/server/ServerNeoDataSource.kt
@@ -0,0 +1,14 @@
+package com.avisual.spaceapp.server
+
+import com.avisual.data.source.NeoRemoteDataSource
+import com.avisual.domain.Neo
+
+class ServerNeoDataSource(private val nasaClient: NasaClient) : NeoRemoteDataSource {
+
+    override suspend fun getNeo(startDate: String, apiKey: String): List<Neo> {
+        return nasaClient.service
+            .searchNeoWsByOnlyStartDate(startDate, apiKey)
+            .toFrameworkNeo()//<-- Mapping to NeoResult to -> FrameworkNeo
+            .map { it.toDomainNeo() }//<-- Mapping to FramworkNeo to Domain
+    }
+}
