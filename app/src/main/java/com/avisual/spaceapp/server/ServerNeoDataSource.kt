e74c531eb1de3d2b1f3ce27cbcd3ec09e7de357c
Arman <arman.agora@gmail.com>
2021-04-25 17:05:02 +0200

    Modified code because it isn't necesary argument in this clase

--- app/src/main/java/com/avisual/spaceapp/server/ServerNeoDataSource.kt
+++ app/src/main/java/com/avisual/spaceapp/server/ServerNeoDataSource.kt
@@ -3,10 +3,10 @@ package com.avisual.spaceapp.server
 import com.avisual.data.source.NeoRemoteDataSource
 import com.avisual.domain.Neo
 
-class ServerNeoDataSource(private val nasaClient: NasaClient) : NeoRemoteDataSource {
+class ServerNeoDataSource : NeoRemoteDataSource {
 
     override suspend fun getAllNeoByDate(startDate: String, apiKey: String): List<Neo> {
-        return nasaClient.service
+        return NasaClient.service
             .searchNeoWsByOnlyStartDate(startDate, apiKey)
             .toFrameworkNeo()//<-- Mapping to NeoResult to -> FrameworkNeo
             .map { it.toDomainNeo() }//<-- Mapping to FramworkNeo to Domain
