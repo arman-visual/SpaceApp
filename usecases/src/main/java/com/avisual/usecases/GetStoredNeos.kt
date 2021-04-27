d50d661553fe4f5590a5690940480c6bacb7ab0a
Arman <arman.agora@gmail.com>
2021-04-27 18:21:18 +0200

    Modified name Fragment in Navigation Button and modified name ofusecase

--- usecases/src/main/java/com/avisual/usecases/GetStoredNeos.kt
+++ usecases/src/main/java/com/avisual/usecases/GetStoredNeos.kt
@@ -4,7 +4,7 @@ import com.avisual.data.repository.NeoRepository
 import com.avisual.domain.Neo
 import kotlinx.coroutines.flow.Flow
 
-class GetAllSavedNeo(private val neoRepository: NeoRepository) {
+class GetStoredNeos(private val neoRepository: NeoRepository) {
 
     fun invoke(): Flow<List<Neo>> = neoRepository.getAllSavedNeo()
 }
