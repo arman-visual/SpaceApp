f8791307bbab054dcd5d838221c90a5725ed2cde
ArmanQT <aquispe@offshoretech.net>
2022-07-07 12:12:14 +0200

    Finished configuration to use test ui with mockwebserver and Idling
    Se ha añadido configuracion para hacer test de interface.
    Añadidas dependencias necesarias y en los scopes necesarios para utilizarlas.

--- app/src/main/java/com/avisual/spaceapp/data/server/NasaClient.kt
+++ app/src/main/java/com/avisual/spaceapp/data/server/NasaClient.kt
@@ -1,19 +1,23 @@
 package com.avisual.spaceapp.data.server
 
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
-class NasaClient(baseUrlApiNasa:String) {
+class NasaClient(baseUrlApiNasa: String) {
 
-    private val clientSetup = OkHttpClient.Builder()
-        .connectTimeout(30, TimeUnit.SECONDS)
-        .writeTimeout(30, TimeUnit.SECONDS)
-        .readTimeout(30, TimeUnit.SECONDS)
-        .build()
+    val clientSetup = HttpLoggingInterceptor().run {
+        level = HttpLoggingInterceptor.Level.BODY
+        OkHttpClient.Builder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .addInterceptor(this).build()
+    }
 
-     val service: NasaService = Retrofit.Builder()
+    val service: NasaService = Retrofit.Builder()
         .baseUrl(baseUrlApiNasa)
         .addConverterFactory(GsonConverterFactory.create())
         .client(clientSetup)
--- app/src/main/java/com/avisual/spaceapp/data/server/NasaGalleryClient.kt
+++ app/src/main/java/com/avisual/spaceapp/data/server/NasaGalleryClient.kt
@@ -1,17 +1,21 @@
 package com.avisual.spaceapp.data.server
 
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
-class NasaGalleryClient(baseUrlNasaImages:String) {
+class NasaGalleryClient(baseUrlNasaImages: String) {
 
-    private val clientSetup = OkHttpClient.Builder()
-        .connectTimeout(30, TimeUnit.SECONDS)
-        .writeTimeout(30, TimeUnit.SECONDS)
-        .readTimeout(30, TimeUnit.SECONDS)
-        .build()
+    val clientSetup = HttpLoggingInterceptor().run {
+        level = HttpLoggingInterceptor.Level.BODY
+        OkHttpClient.Builder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .addInterceptor(this).build()
+    }
 
     val service: NasaLibraryService = Retrofit.Builder()
         .baseUrl(baseUrlNasaImages)
